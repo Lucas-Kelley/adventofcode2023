846cd60d023e71278d1ce5647d5e1e0f83c3055c
Kalon Lucas Kelley <klucas.kelley@gmail.com>
2023-12-02 10:51:02 -0800

    Completed part 2 of day 2

--- day2/cube-conundrum.py
+++ day2/cube-conundrum.py
@@ -3,6 +3,12 @@ lines = fin.read().split('\n')
 
 constraints = {'red': 12, 'green': 13, 'blue': 14}
 
+def get_product(min_set):
+    red = min_set['red']
+    green = min_set['green']
+    blue = min_set['blue']
+    return red*green*blue
+
 total_sum = 0
 for line in lines:
     if not len(line):
@@ -12,15 +18,14 @@ for line in lines:
     game_sets = game_stats[1].strip().split('; ')
     game_list = [{pair[1]:int(pair[0]) for pair in [p.split(' ') for p in elem.split(', ')] } for elem in game_sets]
     bad_game = False
+    min_set = {'red': 0, 'green': 0, 'blue': 0}
     for game_set in game_list:
         for color, value in game_set.items():
-            if value > constraints[color]:
-                bad_game = True
-                break
-        if bad_game:
-            break
-    if bad_game:
-        continue
-    total_sum+=game_number
+            # if value > constraints[color]:
+            #     bad_game = True
+            #     break
+            if value > min_set[color]:
+                min_set[color] = value
+    total_sum+=get_product(min_set)
 
 print(total_sum)
